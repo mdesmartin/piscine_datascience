3d6d2e5a6c5d99fe3213d58f6831733dfe215a0a
Mehdi Desmartin <mdesmart@z3r2p1.42lyon.fr>
2024-12-09 17:34:56 +0100

    modify docker

---
+++ 0_data_engineer/ex02/table.py
@@ -0,0 +1,6 @@
+# create a postgre table with a csv from the customer folder
+# same name for the table without file extension
+# same name for columns as in csv
+# appropriate type (6 differents)
+# DATETIME as first columns
+# check for network in docker-compose
